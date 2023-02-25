7a1133c2fe66cc19b6f07b30a155dc7b1719c97e
Dmitriy Malenik <dmalenik@gmail.com>
2023-02-25 15:34:09 +0100

    feat: implement html webpack plugin configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+// generates its ownn index.html file
+// allows to change dynamically entry points 
+// generates a single html file
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  plugins: [new HtmlWebpackPlugin({ title: 'Output Management' })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
