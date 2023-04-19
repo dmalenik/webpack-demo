38c97a0d2b73c38787c695dc796ad7306e378b32
Dmitriy Malenik <dmalenik@gmail.com>
2023-04-19 14:34:49 +0300

    feat: set up automatic compilation

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,16 @@ module.exports = {
     print: './src/print.js',
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [new HtmlWebpackPlugin({ title: 'Development' })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
