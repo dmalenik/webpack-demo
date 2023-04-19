f1fe268441928016525130b0c1a7320ad4cf93ef
Dmitriy Malenik <dmalenik@gmail.com>
2023-04-19 13:14:04 +0300

    set configs for development mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
-  plugins: [new HtmlWebpackPlugin({ title: 'Output Management' })],
+  devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({ title: 'Development' })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
