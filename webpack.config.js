22c62b1034b4978a25e4ded09d41b223dbc6d712
Dmitriy Malenik <dmalenik@gmail.com>
2023-04-20 15:40:48 +0300

    feat/ code splitting: add dynamic import

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,17 @@
 const path = require('path');
-// generates html bundles automatically and dynamically
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
-  plugins: [new HtmlWebpackPlugin({ title: 'Development' })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
