2fee1561f01fd60b0c1bdbf4d21c2c3456f17de8
Dmitriy Malenik <dmalenik@gmail.com>
2023-04-26 10:49:57 +0300

    feat: add lazy loading code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-async function getComponent() {
-  const element = document.createElement('div');
-  const { default: _ } = await import('lodash');
+import _ from 'lodash';
 
+function component() {
+  const element = document.createElement('div');
+  const button = document.createElement('button');
+  const br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
   element.innerHTML = _.join(['Hello, webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Lazy Loading of the print.js module
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    // reference to default property as it's the actual module object
+    // that will be return as the promise is resolved
+    const print = module.default;
+
+    print();
+  });
 
   return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
