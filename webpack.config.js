d24cabf8a7a87cd7cf4f6ba479f03cd0e8077eff
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-02 12:01:49 +0300

    feat: add changes to webpack configurations
    - rename index key to app key
    - comment print key-value pair out
    - set hot option to true
    - rename title parameter to Hot Module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    app: './src/index.js',
+    // print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+    hot: true,
   },
-  plugins: [new HtmlWebpackPlugin({ title: 'Development' })],
+  plugins: [new HtmlWebpackPlugin({ title: 'Hot Module Replacement' })],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
