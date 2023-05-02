a82f3f80cb2be3c90c4005ef5e09aff65368e7cc
Dmitriy Malenik <dmalenik@gmail.com>
2023-05-02 12:27:36 +0300

    feat: add settings for stylesheets
    - add module options
    - format plugins options

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,23 @@ module.exports = {
   mode: 'development',
   entry: {
     app: './src/index.js',
-    // print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
     hot: true,
   },
-  plugins: [new HtmlWebpackPlugin({ title: 'Hot Module Replacement' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Hot Module Replacement' })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
